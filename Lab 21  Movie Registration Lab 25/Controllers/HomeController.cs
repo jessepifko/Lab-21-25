474de78901891cbd6cdf6d7cecc14afccbffe3f7
Jesse <jessepifko@gmail.com>
2020-05-21 18:23:27 -0400

    Initial commit

---
+++ Lab 21  Movie Registration Lab 25/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab_21__Movie_Registration_Lab_25.Models;
+
+namespace Lab_21__Movie_Registration_Lab_25.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration(Movie movie)
+        {
+            return View(movie);
+        }
+
+        public IActionResult Result(int id, string title, string genre, int year, string actors, string directors)
+        {
+            Movie movie = new Movie(id, title, genre, year, actors, directors);
+
+            if (ModelState.IsValid)
+            {
+                return View(movie);
+            }
+            else
+            {
+                return RedirectToAction("Registration", movie);
+            }
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab 21  Movie Registration Lab 25/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_21__Movie_Registration_Lab_25.Models
+{
+    public class Movie
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Must be less than 50 characters!")]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+      
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(int ID, string Title, string Genre, int Year, string Actors, string Directors)
+        {
+            this.ID = ID;
+            this.Title = Title;
+            this.Genre = Genre;
+            this.Year = Year;
+            this.Actors = Actors;
+            this.Directors = Directors;
+        }
+    }
+}
